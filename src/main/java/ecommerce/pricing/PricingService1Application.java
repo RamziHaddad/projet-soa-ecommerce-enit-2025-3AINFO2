aa06de3f9efae8d7c5ab2dc6c319a96c83a8abdb
amani.benmesmiaàetudiant-enit.utm.tn <amani.benmesmiaetudiant-enit.utm.tn>
2025-12-02 22:28:11 +0100

    Add the remaining kafka integration files

--- src/main/java/ecommerce/pricing/PricingService1Application.java
+++ src/main/java/ecommerce/pricing/PricingService1Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PricingService1Application {
+public class  PricingService1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PricingService1Application.class, args);
